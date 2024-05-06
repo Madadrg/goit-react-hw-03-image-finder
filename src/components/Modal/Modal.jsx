eaed233e5acf6b8ff88ceb5c9cc89b98c7fa280f
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-05-07 01:10:38 +0300

    Removed error

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 
 const Modal = ({ imageUrl, alt, onClose }) => {
-  const handleKeyDown = event => {
-    if (event.keyCode === 27) {
-      onClose();
-    }
-  };
-
   const handleClickOutside = event => {
     if (event.target.classList.contains('overlay')) {
       onClose();
@@ -14,11 +8,17 @@ const Modal = ({ imageUrl, alt, onClose }) => {
   };
 
   useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.keyCode === 27) {
+        onClose();
+      }
+    };
+
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]); // Include handleKeyDown in the dependency array
+  }, [onClose]);
 
   return (
     <div className="overlay" onClick={handleClickOutside}>
